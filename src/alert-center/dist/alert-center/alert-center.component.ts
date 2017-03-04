840f97753ba98f3698ec8c1a34f2d3f958509887
zsfarkas <github@farkas.land>
2017-03-04 09:53:34 +0100

    first draft of animation (not working and commented)

--- src/alert-center/dist/alert-center/alert-center.component.ts
+++ src/alert-center/dist/alert-center/alert-center.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit, trigger, state, transition, style, animate} from '@angular/core';
 import {AlertCenterService} from '../service/alert-center.service';
 import {Alert} from '../model/alert';
 
@@ -7,12 +7,19 @@ import {Alert} from '../model/alert';
   template: `
     <div class="alert-list">
       <nac-alert *ngFor="let alert of alerts" [alert]="alert" (dismissed)="remove(alert)"></nac-alert>
+      <!--nac-alert *ngFor="let alert of alerts" [@inOut] [alert]="alert" (dismissed)="remove(alert)"></nac-alert-->
     </div>
   `,
-  styles: [
-    `
-
-    `
+  animations: [
+    trigger('inOut', [
+      //state('in', style({transform: 'scale(1.0)'})),
+      transition('void => *', [
+        animate(500, style({transform: 'scale(1.0)'}))
+      ]),
+      transition('* => void', [
+        animate(500, style({transform: 'scale(0.0)'}))
+      ])
+    ])
   ]
 })
 export class AlertCenterComponent implements OnInit {
