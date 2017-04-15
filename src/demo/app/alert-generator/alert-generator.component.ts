13160b53aca38c56c1ceafec0b1eb8a428373c75
zsfarkas <github@farkas.land>
2017-04-15 19:30:12 +0200

    show case extended for animation

--- src/demo/app/alert-generator/alert-generator.component.ts
+++ src/demo/app/alert-generator/alert-generator.component.ts
@@ -6,8 +6,8 @@ import {Alert} from '../modules';
 @Component({
   selector: 'nac-alert-generator',
   template: `
-<div class="alert-container">
-  <nac-alert-center [animation]="'fancy'"></nac-alert-center>
+<div class="alert-container" [style.left]="getLeft()" [style.right]="getRight()">
+  <nac-alert-center [animation]="animation"></nac-alert-center>
 </div>
 <div class="container">
   <br><br>
@@ -20,17 +20,18 @@ import {Alert} from '../modules';
         <div class="panel-body">
             <form>
               <div class="form-group">
-                <label for="alert-text">Alert text</label>
+                <label for="alert-text">Alert text (text)</label>
                 <input class="form-control" id="alert-text" type="text" name="alertText" [(ngModel)]="alert.text">
               </div>
               <div class="form-group">
-                <label for="strong-text">Strong text</label>
+                <label for="strong-text">Strong text (textStrong)</label>
                 <input class="form-control" id="strong-text" type="text" name="strongText" [(ngModel)]="alert.textStrong">
               </div>
               <div class="form-group">
-                <label for="dismiss-time">Dismiss Time</label>
+                <label for="dismiss-time">Dismiss Time (dismissTime)</label>
                 <input class="form-control" id="dismiss-time" type="text" name="dismissTime" [(ngModel)]="alert.dismissTime">
               </div>
+              <label>Alert type (AlertType)</label>
               <div class="form-group">
                 <label class="radio-inline">
                   <input type="radio" name="alertType" [value]="0" [(ngModel)]="alert.alertType">SUCCESS
@@ -43,6 +44,31 @@ import {Alert} from '../modules';
                 </label>
                 <label class="radio-inline">
                   <input type="radio" name="alertType" [value]="3" [(ngModel)]="alert.alertType">DANGER
+                </label>
+              </div>
+              <hr>
+              <label>Animation</label>
+              <div class="form-group">
+                <label class="radio-inline">
+                  <input type="radio" name="animation" [value]="'none'" [(ngModel)]="animation">No animation ('none')
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="animation" [value]="'decent'" [(ngModel)]="animation">Decent animation ('decent')
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="animation" [value]="'fancy'" [(ngModel)]="animation">Fancy animation ('fancy')
+                </label>
+              </div>
+              <label>Alert center alignment</label>
+              <div class="form-group">
+                <label class="radio-inline">
+                  <input type="radio" name="align" [value]="0" [(ngModel)]="align">Align left
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="align" [value]="1" [(ngModel)]="align">Align center
+                </label>
+                <label class="radio-inline">
+                  <input type="radio" name="align" [value]="2" [(ngModel)]="align">Align right
                 </label>
               </div>
             </form>
@@ -69,6 +95,9 @@ import {Alert} from '../modules';
 export class AlertGeneratorComponent implements OnInit {
   alert: Alert = new Alert(AlertType.SUCCESS, AlertGeneratorComponent.createTestMessage(), 'CALCULATION: ');
 
+  animation = 'fancy';
+  align = 2;
+
   constructor(private alertCenterService: AlertCenterService) {
   }
 
@@ -78,6 +107,28 @@ export class AlertGeneratorComponent implements OnInit {
   sendAlert() {
     this.alertCenterService.alert(this.alert);
     this.alert = new Alert(this.alert.alertType, AlertGeneratorComponent.createTestMessage(), this.alert.textStrong, this.alert.dismissTime);
+  }
+
+  getLeft() {
+    switch (this.align) {
+      case 0:
+        return '0';
+      case 1:
+        return '20%';
+      case 2:
+        return '60%';
+    }
+  }
+
+  getRight() {
+    switch (this.align) {
+      case 0:
+        return '60%';
+      case 1:
+        return '20%';
+      case 2:
+        return '0';
+    }
   }
 
   private static createTestMessage() {
@@ -91,5 +142,4 @@ export class AlertGeneratorComponent implements OnInit {
   private static createRandomNumberBetweenNullAndTentausend() {
     return Math.round(Math.random() * 10000);
   }
-
 }
